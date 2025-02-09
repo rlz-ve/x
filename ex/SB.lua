8345e70bf19f05b86bebe3d802bdb1805262e0f0
rlz-ve <rizveholder@gmail.com>
2025-02-08 23:51:52 -0500

    Create SB.lua

---
+++ ex/SB.lua
@@ -0,0 +1,16 @@
+-- Tested in 'a literal baseplate.'
+local function cStr(str: string) : string
+    return str:gsub('\0', '')
+end
+
+local targetScript = game.Players.LocalPlayer.PlayerScripts.LocalScript
+
+print("Original Bytecode:", cStr(getscriptbytecode(targetScript)))
+
+setscriptbytecode(targetScript, Compile("print('Hello, World!')"))
+
+print("New Bytecode:", cStr(getscriptbytecode(targetScript)))
+
+restorescriptbytecode(targetScript) -- Can still be called after Xeno has restarted due to cache system
+
+print("Restored Bytecode:", cStr(getscriptbytecode(targetScript)))
